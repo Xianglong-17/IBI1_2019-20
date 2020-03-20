4ee581850fc4d1ca6a9b52adeeba080abf9d9125
1 <xianglong.19@intl.zju.edu.cn>
2020-03-20 13:03:31 +0800

    new version

--- Practical5/collatz.py
+++ Practical5/collatz.py
@@ -1,8 +1,9 @@
 
 x=63 #have an integer x
-while x!=1:#if x is even,devided by 2; if odd, multipy by 3 and add 1;if x=1, stop.
+while x!=1:
+    print(x)#if x is even,devided by 2; if odd, multipy by 3 and add 1;if x=1, stop.
     if x%2==0:
         x=x/2
-    else:
+    elif x%2==1:
         x=x*3+1
 print(x)
